15860e8476b93f96d357d6ff92b4b676aa61442d
Admin <Admin@28122017KP1>
2023-04-25 17:07:36 +0300

    Add project files.

---
+++ Triugulnik/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triugulnik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter a side of the triangle : ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Enter the triangle's height : ");
+            double h = double.Parse(Console.ReadLine());
+            Triangle triangle = new Triangle(a, h);
+            Console.WriteLine($"Триъгълник e със страна --> {triangle.PrintA()}, и височина, която е към нея  --> {triangle.PrintH()}");
+            triangle.Print();
+
+        }
+    }
+}
---
+++ Triugulnik/Triangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triugulnik
+{
+    internal class Triangle
+    {
+        private double a;
+        private double h;
+        public double A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        public double H
+        {
+            get { return h; }
+            set { h = value; }
+        }
+        public Triangle(double a, double h)
+        {
+            this.a = a;
+            this.h = h;
+        }
+        public double FaceOfTriangle()
+        {
+            double result;
+            result = a * h / 2;
+            return result;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Страна = {a} и височина към нея = {h} , са с лице {FaceOfTriangle()}");
+        }
+        public double PrintA()
+        {
+            return a;
+        }
+        public double PrintH()
+        {
+            return h;
+        }
+    }
+}
